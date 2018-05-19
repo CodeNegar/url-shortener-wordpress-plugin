18269ccb63f594e7a248aaab23c54560625a8353
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-20 00:27:08 +0800

    scaffold plugin tests with wp-cli

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Url_Shortener
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
