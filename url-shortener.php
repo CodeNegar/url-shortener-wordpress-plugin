f876b4b36a2f40580a242ac2a93d68d4734df5fb
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-05-20 13:22:35 +0800

    Base structure created

---
+++ url-shortener.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name:       URL shortener
+ * Plugin URI:        https://github.com/CodeNegar/url-shortener-wordpress-plugin
+ * Description:       This is a simple WordPress plugin for https://github.com/CodeNegar/url-shortener
+ * Version:           1.0.0
+ * Author:            Farhad Ahmadi
+ * License:           GPL-2.0+
+ * Based on:          wppb.io Boilerplate
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       url-shortener
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
